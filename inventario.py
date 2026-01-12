93b4682fa6d5757ab65d33f2620e3f25a2d769ba
Rubén Oronoz <oronozruben37@gmail.com>
2026-01-12 03:12:51 -0400

    Update inventario.py

--- inventario.py
+++ inventario.py
@@ -1,83 +1,85 @@
-productos = []
+class Inventario:
+	def __init__(self):
+		self.inventario= []
+		self.cargar_archivo()
+		
+	def agregar_producto(self):
+		nombre = input("Nombre: ").title()
+		if self.producto_existe(nombre):
+    print("ERROR: El producto ya existe")
+    return
+		if nombre == "":
+			print("ERROR: Nombre vacio")
+			return
+			
+		try:
+			precio = float(input("Precio: "))
+			stock = int(input("Stock: "))
+			
+			if precio < 0 or stock < 0:
+				print("ERROR: Numero invalido")
+				return
+				
+			producto = {
+			'nombre': nombre,
+			'precio': precio,
+			'stock': stock
+			}
+			self.inventario.append(producto)
+			self.guardar_archivo()
+			print("Producto agregado con exito")
+			
+		except ValueError:
+			print("ERROR: Debe ingresar valores numericos en Precio y Stock")
+				
+	def mostrar_productos(self):
+		if not self.inventario:
+			print("Inventario vacio")
+			return
+		
+		for p in self.inventario:
+			print(f"{p['nombre']} | {p['precio']}$ | {p['stock']}")
+			
+	def buscar_producto(self):
+		nombre = input("Nombre del producto a buscar: ").title()
+		
+		for p in self.inventario:
+			if p['nombre'] == nombre:
+				print(p)
+				return
+		print("No encontrado")			
+	
+	def eliminar_producto(self):
+    nombre = input("Nombre del producto a eliminar: ").title()
 
-def agregar_producto():
-    nombre = input("Nombre: ").title()
-    if nombre == "":
-        print("No puedes dejar el nombre vacío")
-        return
-
-    try:
-        precio = float(input("Precio: "))
-        stock = int(input("Stock: "))
-
-        if precio < 0:
-            print("ERROR: El precio no puede ser negativo")
-        elif stock <= 0:
-            print("ERROR: El stock debe ser mayor a cero")
-        else:
-            productos.append({
-                "nombre": nombre,
-                "precio": precio,
-                "stock": stock
-            })
-            print("Producto registrado")
-    except ValueError:
-        print("ERROR: Solo debes introducir valores numéricos")
-
-def mostrar_productos():
-    if not productos:
-        print("No hay registro de productos")
-        return
-
-    for p in productos:
-        print(f"Nombre: {p['nombre']} | Precio: {p['precio']} | Stock: {p['stock']}")
-
-def buscar_productos():
-    if not productos:
-        print("No hay registro de productos")
-        return
-
-    buscado = input("Producto a buscar: ").title()
-    encontrado = False
-
-    for p in productos:
-        if p["nombre"] == buscado:
-            print(f"Nombre: {p['nombre']} | Precio: {p['precio']} | Stock: {p['stock']}")
-            encontrado = True
-
-    if not encontrado:
-        print("No hay registros con ese nombre")
-
-def eliminar_producto():
-    if not productos:
-        print("No hay registro de productos")
-        return
-
-    eliminado = input("Producto a eliminar: ").title()
-
-    for p in productos[:]:
-        if p["nombre"] == eliminado:
-            permiso = input("Seguro que desea eliminarlo? (s/n): ")
-            if permiso == "s":
-                productos.remove(p)
-                print("Producto eliminado")
-                return
-            elif permiso == "n":
+    for p in self.inventario:
+        if p["nombre"] == nombre:
+            confirmacion = input("¿Seguro que desea eliminarlo? (s/n): ").lower()
+            if confirmacion == "s":
+                self.inventario.remove(p)
+                self.guardar_archivo()
+                print("Producto eliminado con éxito")
+            else:
                 print("Eliminación cancelada")
-                return
+            return
 
-    print("No hay registros con ese nombre")
-
-def resumen_inventario():
-    if not productos:
-        print("No hay registro de productos")
-        return
-
-    total_productos = len(productos)
-    total_inventario = 0
-
-    for p in productos:
-        total_inventario += p["precio"] * p["stock"]
-
-    print(f"Total de productos: {total_productos}")
-    print(f"Valor total del inventario: {total_inventario}$")
+    print("No encontrado")
+    	
+	def guardar_archivo(self):
+		with open("inventario.txt", "w") as archivo:
+				for p in self.inventario:
+					archivo.write(f"{p['nombre']},{p['precio']},{p['stock']}\n")
+				
+	def cargar_archivo(self):
+		self.inventario.clear()
+		try:
+			with open("inventario.txt", "r") as archivo:
+				for linea in archivo:
+					nombre, precio, stock = linea.strip().split(",")
+					self.inventario.append({
+					'nombre': nombre,
+					'precio': float(precio),
+					'stock': int(stock)
+					})
+		except FileNotFoundError:
+			pass
