59e9d558dbb068699c8e8066333a74d49474213a
Rubén Oronoz <oronozruben37@gmail.com>
2026-01-12 03:13:55 -0400

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,40 +1,27 @@
-from inventario import (
-    agregar_producto,
-    mostrar_productos,
-    buscar_productos,
-    eliminar_producto,
-    resumen_inventario
-)
+from inventario import Inventario
 
-def menu():
-    while True:
-        print("==== MENU INVENTARIO ====")
-        print("1. Agregar producto")
-        print("2. Mostrar productos")
-        print("3. Buscar producto")
-        print("4. Eliminar producto")
-        print("5. Resumen del inventario")
-        print("6. Salir")
+inv = Inventario()
 
-        opcion = input("Elegir opción: ")
-
-        if opcion == "1":
-            agregar_producto()
-        elif opcion == "2":
-            mostrar_productos()
-        elif opcion == "3":
-            buscar_productos()
-        elif opcion == "4":
-            eliminar_producto()
-        elif opcion == "5":
-            resumen_inventario()
-        elif opcion == "6":
-            print("Saliendo del sistema...")
-            break
-        else:
-            print("Opción inválida")
-
-def main():
-    menu()
-
-main()
+while True:
+	print("~~~~MENU~~~~")
+	print("1. Agregar Producto")
+	print("2. Mostrar Productos")
+	print("3. Buscar Producto")
+	print("4. Eliminar Producto")
+	print("5. Salir")
+		
+	opcion = input("Elegir: ")
+		
+	if opcion == "1":
+		inv.agregar_producto()
+	elif opcion == "2":
+		inv.mostrar_productos()
+	elif opcion == "3":
+		inv.buscar_producto()
+	elif opcion == "4":
+		inv.eliminar_producto()
+	elif opcion == "5":
+		print("Inventario guardado, hasta luego...")
+		break	
+	else:
+		print("Opcion invalida")
